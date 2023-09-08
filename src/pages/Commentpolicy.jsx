6a2ec31acc3ed17ff2a08174d02c17dc40760b52
amaniintare <amanimae2@gmail.com>
2023-09-08 15:15:10 +0200

    Adding react js files and strapi files! disqus comments included with unworking searchbar

---
+++ src/pages/Commentpolicy.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Commentpolicy = () => {
+  return (
+    <div>
+      <p>
+please respect the rules here. no abusive comments. everyone should respect anyone comment and vice versa. thank you
+      </p>
+    </div>
+  );
+};
+
+export default Commentpolicy;
